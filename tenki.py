b3e8685c859e083deb31327737820b24c0d89211
Poshen100 <128558874+Poshen100@users.noreply.github.com>
2025-10-15 11:02:44 +0800

    add my first tenki.a code

---
+++ tenki.py
@@ -0,0 +1,19 @@
+name: CI
+
+on:
+  push:
+    branches: [ main ]
+
+jobs:
+  test:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v3
+      - name: Set up Flutter
+        uses: subosito/flutter-action@v2
+        with:
+          flutter-version: '3.24.0'
+      - name: Install dependencies
+        run: flutter pub get
+      - name: Run tests
+        run: flutter test
